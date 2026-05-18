e7670294abfc33e87d179514a015a623c7b383c5
Jitesh Mittal <jiteshmittal2006@gmail.com>
2026-05-18 10:43:16 +0530

    Time: 72 ms (85.40%) | Memory: 75.1 MB (82.36%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 1){
+            return 0;
+        }
+
+        unordered_map<int , vector<int>> jumps;
+
+        for (int i = 0; i<n ; i++){
+            jumps[arr[i]].push_back(i);
+        }
+
+        queue<int> q;
+
+        vector<bool> visited(n,false); 
+        q.push(0);
+        visited[0] = true;
+
+        int steps = 0;
+
+        while(!q.empty()){
+            int size = q.size();
+
+            while(size--){
+                int idx = q.front();
+                q.pop();
+
+                if (idx == n-1){
+                    return steps;
+                }
+
+                if (idx - 1 >= 0 && !visited[idx - 1]) {
+                    visited[idx - 1] = true;
+                    q.push(idx - 1);
+                }
+                if (idx + 1 < n && !visited[idx + 1]) {
+                    visited[idx + 1] = true;
+                    q.push(idx + 1);
+                }
+                for (auto next : jumps[arr[idx]]) {
+
+                    if (!visited[next]) {
+                        visited[next] = true;
+                        q.push(next);
+                    }
+                }
+
+                jumps[arr[idx]].clear();
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
