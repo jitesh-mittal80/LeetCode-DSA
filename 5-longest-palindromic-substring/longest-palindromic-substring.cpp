599ef9f30a3fd273e28eff4594405d937b531eaa
Jitesh Mittal <jiteshmittal2006@gmail.com>
2026-08-15 09:59:14 +0530

    Time: 358 ms (13.06%) | Memory: 292.2 MB (5.73%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        int start = 0;
+        int mlen = 1;
+        for(int i = 0; i<n; i++){
+            for(int j = i; j<n; j++){
+                if(ispal(s, i, j, dp)){
+                    if(j-i+1 > mlen){
+                        mlen = j-i+1;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substr(start, mlen);
+    }
+
+    bool ispal(string& s, int i, int j, vector<vector<int>>& dp){
+        if(i >= j){
+            return true;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(s[i] != s[j]){
+            return dp[i][j] = false;
+        }
+        return dp[i][j] = ispal(s, i+1, j-1, dp);
+    }
+};
