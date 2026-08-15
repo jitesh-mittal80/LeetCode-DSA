8d4b67e6645374f35c12badcc3f71e7872d9e59a
Jitesh Mittal <jiteshmittal2006@gmail.com>
2026-08-15 17:03:30 +0530

    Time: 426 ms (11.50%) | Memory: 292.5 MB (5.66%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -6,7 +6,7 @@ public:
         int start = 0;
         int mlen = 1;
         for(int i = 0; i<n; i++){
-            for(int j = i; j<n; j++){
+            for(int j = n-1; j >=0; j--){
                 if(ispal(s, i, j, dp)){
                     if(j-i+1 > mlen){
                         mlen = j-i+1;
